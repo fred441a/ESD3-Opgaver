01130f9ce3c0914c6083b57b25e2810898039977
Mikkel <mikkel@norre.cc>
2023-09-14 13:52:24 +0200

    Renamed digital folder

---
+++ DigitalHardwareDesign/KG4/EX3/EX3.srcs/sources_1/new/4-bit-buffer.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 09/14/2023 12:50:13 PM
+-- Design Name: 
+-- Module Name: 4-bit-buffer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Bit4Buffer is
+    Port ( input_bus : in STD_LOGIC_VECTOR (3 downto 0);
+           output_bus : out STD_LOGIC_VECTOR (3 downto 0);
+           clk : in STD_LOGIC;
+           WE : in STD_LOGIC;
+           RE : in STD_LOGIC);
+end Bit4Buffer;
+
+architecture Behavioral of Bit4Buffer is
+signal internal_buffer : std_logic_vector (3 downto 0);
+begin
+
+process (clk)
+begin  
+   if (clk'event and clk = '1') then
+      if WE = '1' then
+         internal_buffer (3 downto 0) <= input_bus (3 downto 0);
+      elsif RE = '1' then
+         output_bus (3 downto 0) <= internal_buffer (3 downto 0);      
+      end if;
+   end if;
+end process;
+
+end Behavioral;
