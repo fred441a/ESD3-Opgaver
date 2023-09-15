ad2ec2f1724724de8ad2d94be5de5b0eb4a9fb3c
Frederik Hansen <frederik@altofte.dk>
2023-09-15 12:00:01 +0200

    exersice 1 kode

--- Algorithms/KG2/insertsort.py
+++ Algorithms/KG2/insertsort.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mlp
 import random
+import time
 
 def insert_sort (inputList):
     for j in range(1,len(inputList)):
@@ -19,11 +20,35 @@ def generate_random_list(n):
         return_list.append(random.random()*10)
     return return_list
 
+def reverse_list(in_list):
+    return_list = []
+    for i in reversed(range(len(in_list))):
+        print(i) #DO NOT DELETE THIS PRINT STATEMENT!
+        #for some reason this cursed shit language need it to work
+        return_list.append(in_list[i]);
+    return return_list
+
 time_list = []
 for i in range(4):
     random_list = generate_random_list(4)
+    print(random_list)
+    start_time = time.time()
+    insert_sort(random_list)
+    end_time = time.time()
+    time_list.append(end_time-start_time)
 
+    reversed_random_list = reverse_list(random_list)
+    print(reversed_random_list)
 
+    start_time = time.time()
+    insert_sort(reversed_random_list)
+    end_time = time.time()
+    time_list.append(end_time-start_time)
 
+print(time_list)
+
+fig, ax = plt.subplots()
+ax.bar(range(len(time_list)),time_list)
+plt.show()
 
 
