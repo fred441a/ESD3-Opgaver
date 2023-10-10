3becc1089c0be86dabe1451b18328594f281a401
Frederik Hansen <frederik@altofte.dk>
2023-10-10 18:43:03 +0200

    fixed rand and run times in main

--- Algorithms/KG4/Main.c
+++ Algorithms/KG4/Main.c
@@ -13,24 +13,30 @@ void testArray(int arraySize, void (*testFunc)(int*,int)){
 		for(int i=0; i < arraySize; i++){
 				sortedArray[i] = i;
 				reverseSortedArray[i] = arraySize-i;
-				randomArray[i] = rand();
+				randomArray[i] = (rand()/RAND_MAX)*100;
 		}
 
 		unsigned long time; 
 
 		time = clock();
+		for(int i = 0; i < 100; i++){
 		testFunc(sortedArray,arraySize);
-		printf("sorted array: %f\n",time-clock()/(double)CLOCKS_PER_SEC);
+		}
+		printf("sorted array: %f\n",(time-clock()/100.0)/1000000);
 		time = clock();
-
+		
 		time = clock();
+		for(int i = 0; i < 100; i++){
 		testFunc(reverseSortedArray,arraySize);
-		printf("reversed array: %f\n",time-clock()/(double)CLOCKS_PER_SEC);
+		}
+		printf("reversed array: %f\n",(time-clock()/100.0)/1000000);
 		time = clock();
 
 		time = clock();
+		for(int i = 0; i < 100; i++){
 		testFunc(randomArray,arraySize);
-		printf("random array: %f\n",time-clock()/(double)CLOCKS_PER_SEC);
+		}
+		printf("random array: %f\n",(time-clock()/100.0)/1000000);
 		time = clock();
 
 }
@@ -38,20 +44,32 @@ void testArray(int arraySize, void (*testFunc)(int*,int)){
 int main(){
 
 		printf("100\n");
+		printf("Quick sort \n");
 		testArray(100,RealQuickSort);
+		printf("Heap sort \n");
 		testArray(100,heapSort);
+		printf("Counting sort\n");
 		testArray(100,CountingSort);
 		printf("1000\n");
+		printf("Quick sort \n");
 		testArray(1000,RealQuickSort);
+		printf("Heap sort \n");
 		testArray(1000,heapSort);
+		printf("Counting sort\n");
 		testArray(1000,CountingSort);
 		printf("10000\n");
+		printf("Quick sort \n");
 		testArray(10000,RealQuickSort);
+		printf("Heap sort \n");
 		testArray(10000,heapSort);
+		printf("Counting sort\n");
 		testArray(10000,CountingSort);
     	printf("100000\n");
+		printf("Quick sort \n");
 		testArray(100000,RealQuickSort);
+		printf("Heap sort \n");
 		testArray(100000,heapSort);
+		printf("Counting sort\n");
 		testArray(100000,CountingSort);	
 		return 0;
 }
