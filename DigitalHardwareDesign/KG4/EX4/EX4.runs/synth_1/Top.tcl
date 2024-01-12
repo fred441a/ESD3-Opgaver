3e0b86d8032bcd51d41199e32d89f000f297ce35
MhawnSendes <jtheil22@student.aau.dk>
2024-01-12 10:02:57 +0100

    Jeg ved ikke hvad det her er men det er unstaged

--- DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1/Top.tcl
+++ DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1/Top.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "/home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1/Top.tcl"
+  variable script "C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1/Top.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,30 +70,23 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param chipscope.maxJobs 3
-set_param checkpoint.writeSynthRtdsInDcp 1
-set_param synth.incrementalSynthesisCache ./.Xil/Vivado-4042-Lenova/incrSyn
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7a35tcpg236-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.cache/wt [current_project]
-set_property parent.project_path /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.cache/wt [current_project]
+set_property parent.project_path C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language VHDL [current_project]
-set_property board_part_repo_paths {/home/fred441a/.Xilinx/Vivado/2023.1/xhub/board_store/xilinx_board_store} [current_project]
-set_property board_part digilentinc.com:cmod_a7-35t:part0:1.2 [current_project]
-set_property ip_output_repo /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.cache/ip [current_project]
+set_property ip_output_repo c:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_vhdl -library xil_defaultlib {
-  /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/sources_1/new/test.vhd
-  /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/sources_1/new/Top.vhd
+  C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/sources_1/new/test.vhd
+  C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/sources_1/new/Top.vhd
 }
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -104,12 +97,12 @@ OPTRACE "Adding files" END { }
 foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
   set_property used_in_implementation false $dcp
 }
-read_xdc /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/constrs_1/new/Top_Constraints.xdc
-set_property used_in_implementation false [get_files /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/constrs_1/new/Top_Constraints.xdc]
+read_xdc C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/constrs_1/new/Top_Constraints.xdc
+set_property used_in_implementation false [get_files C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/constrs_1/new/Top_Constraints.xdc]
 
 set_param ips.enableIPCacheLiteLoad 1
 
-read_checkpoint -auto_incremental -incremental /home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/utils_1/imports/synth_1/Top.dcp
+read_checkpoint -auto_incremental -incremental C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.srcs/utils_1/imports/synth_1/Top.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
--- DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1/runme.sh
+++ DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1/runme.sh
@@ -7,10 +7,14 @@
 # Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 # 
 
+echo "This script was generated under a different operating system."
+echo "Please update the PATH and LD_LIBRARY_PATH variables below, before executing this script"
+exit
+
 if [ -z "$PATH" ]; then
-  PATH=/tools/Xilinx/Vivado/2023.1/ids_lite/ISE/bin/lin64:/tools/Xilinx/Vivado/2023.1/bin
+  PATH=C:/Xilinx/Vivado/2023.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2023.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2023.1/bin
 else
-  PATH=/tools/Xilinx/Vivado/2023.1/ids_lite/ISE/bin/lin64:/tools/Xilinx/Vivado/2023.1/bin:$PATH
+  PATH=C:/Xilinx/Vivado/2023.1/ids_lite/ISE/bin/nt64;C:/Xilinx/Vivado/2023.1/ids_lite/ISE/lib/nt64:C:/Xilinx/Vivado/2023.1/bin:$PATH
 fi
 export PATH
 
@@ -21,7 +25,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='/home/fred441a/Documents/Uni/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1'
+HD_PWD='C:/Users/theil/ESD3-Opgaver/DigitalHardwareDesign/KG4/EX4/EX4.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
